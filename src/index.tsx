10613cb8953cc7096c58c0dd02c54397405e9181
Alex <aironer@yandex.ru>
2024-03-14 02:02:01 +0300

    start template3

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import "./index.css"
 import App from "./App"
 import GlobalStyles from "./components/globalStyles/globalStyles"
 import { HashRouter } from "react-router-dom"
